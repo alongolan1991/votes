a5872cfc9881cc14613650159a3787a4e8d3d984
Alon Golan <eggnoob@gmail.com>
2018-05-10 14:19:26 +0300

    last change

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ http.createServer(function(req,res){
   res.writeHead(200);
   res.write(v1.logarray.toString());
   res.end();
-})..listen(process.env.PORT || 3000);;
+}).listen(process.env.PORT || 3000);
 console.log('listening on port 3000');
